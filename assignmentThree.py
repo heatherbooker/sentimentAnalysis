114403d23c2cb81802ea839ef85118e0373635b0
Heather Booker <heather.booker@hotmail.com>
2015-11-02 00:55:04 -0500

    completely functional except for file submission error handling

--- assignmentThree.py
+++ assignmentThree.py
@@ -1,5 +1,9 @@
-#filename = input('tweets file name: ')
-tweetsData = open('tweets.txt')
+tweetsFile = input('tweets file name: ')
+try:
+	open(tweetsFile)
+except:
+	pass
+# tweetsData = open('tweets.txt')
 #filename2 = input('keywords file name: ')
 keywordsData = open('keys.txt')
 
@@ -12,8 +16,6 @@ def extractTweet(line) :
 	tweet = tupl[2].lower()
 	#date and time make 22 characters, so take string starting after them to the end
 	tweet = tweet[23:].rstrip()
-	# print(latLon)
-	print(tweet)
 	return (latLon, tweet)
 
 
@@ -24,7 +26,6 @@ def setUpKeywords(keywordsFile) :
 		keysAndValues = line.split(',')
 		keywords.append(keysAndValues[0])
 		values.append(int(keysAndValues[1]))
-	print(keywords)
 	return (keywords, values)
 
 
@@ -37,19 +38,10 @@ def searchTweet(tweet, keysAndValues) :
 		if word in tweet:
 			wordsFound.append(word)
 			happiness += values[index]
-	if wordsFound:
-		print(wordsFound)
-	if happiness:
-		# print(happiness)
-		pass
-	print('')
+	return(happiness)
+
 
 def findTimeZone(latLon) :
-	#do i even need these?
-	# pacific = (-125.242264, -115.236428)
-	# mountain = (-115.236428, -101.998892)
-	# central = (-101.998892, -87.518395)
-	# eastern = (-87.518395, -67.444574)
 	lat = float(latLon[0])
 	lon = float(latLon[1])
 	if lat > 49.189787 or lat < 24.660845 or lon < -125.242264 or lon > -67.444574:
@@ -57,33 +49,44 @@ def findTimeZone(latLon) :
 		print('***location not within program constraints***')
 		return False
 	elif -87.518395 < lon <= -67.444574:
-		# print('EASTERN TIME ZONE')
 		return 'eastern'
 	elif -101.998892 < lon <= -87.518395:
-		# print('CENTRAL TIME ZONE')
 		return 'central'
 	elif -115.236428 < lon <= -101.998892:
-		# print('MOUNTAIN TIME ZONE')
 		return 'mountain'
 	elif -125.242264 <= lon <= -115.236428:
-		# print('PACIFIC TIME ZONE')
 		return 'pacific'
+
+def increment(zone, happiness) :
+	zone[1] += 1
+	zone[2] += happiness
+	return zone
+
+def report(info) :
+	print('for the {} time zone: {} tweets and a happiness score of {}'.format(info[0], info[1], info[2]))
 
 def main() :
 	keys = setUpKeywords(keywordsData)
+	eastern = ['eastern', 0, 0]
+	central = ['central', 0, 0]
+	mountain = ['mountain', 0, 0]
+	pacific = ['pacific', 0, 0]
 	for line in tweetsData:
 		data = extractTweet(line)
 		timeZone = findTimeZone(data[0])
-		searchTweet(data[1], keys)
+		happiness = searchTweet(data[1], keys)
+		if timeZone == 'eastern':
+			increment(eastern, happiness)
+		if timeZone == 'central':
+			increment(central, happiness)
+		if timeZone == 'mountain':
+			increment(mountain, happiness)
+		if timeZone == 'pacific':
+			increment(pacific, happiness)
+	# print(eastern, central, mountain, pacific)
+	report(eastern)
+	report(central)
+	report(mountain)
+	report(pacific)
 
 main()
-# p1 = (49.189787, -67.444574)
-# p2 = (24.660845, -67.444574)
-# p3 = (49.189787, -87.518395)
-# p4 = (24.660845, -87.518395)
-# p5 = (49.189787, -101.998892)
-# p6 = (24.660845, -101.998892)
-# p7 = (49.189787, -115.236428)
-# p8 = (24.660845, -115.236428)
-# p9 = (49.189787, -125.242264)
-# p10 = (24.660845, -125.242264)
